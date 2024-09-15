e9b1fd17b9db9e43924c9bb9292c5c8fc130d52f
mighT77 <142781273+mighT77@users.noreply.github.com>
2024-09-15 21:24:59 +0800

    Added comments

--- camera_calibration/GSMLCams/GSMLCams/sub_separate.py
+++ camera_calibration/GSMLCams/GSMLCams/sub_separate.py
@@ -8,14 +8,14 @@ import os
 # extends node acting as ROS2 subscriber for images
 class CameraImageSubscriber(Node):
     def __init__(self):
-        super().__init__('camera_image_subscriber')
+        super().__init__('camera_image_subscriber')        #initialise nose with name
 
         # Create a subscription to the image topic, listening on the '/video_frames' topic
         self.subscription = self.create_subscription(
             Image,
-            '/video_frames',  # Replace with image topic
+            '/video_frames',  # image topic name
             self.listener_callback,
-            10
+            10        # Queue size for storing images
         )
 
         self.bridge = CvBridge()        # Initialize the CvBridge object to convert ROS Image messages to OpenCV images
@@ -25,18 +25,27 @@ class CameraImageSubscriber(Node):
     
     # Callback function that gets triggered when an image message is received
     def listener_callback(self, msg):
+        # Convert the ROS Image message to an OpenCV image using CvBridge
         cv_image = self.bridge.imgmsg_to_cv2(msg)
-        image_filename = f'calibration_image_{self.image_counter}.jpg'        # Create a filename for the saved image, using the image counter to ensure unique filenames
-        os.makedirs("pictures", exist_ok=True)                                # Ensure that the 'pictures' directory exists, create it if it doesn't
+        # Generate a unique numbered filename for the saved image
+        image_filename = f'calibration_image_{self.image_counter}.jpg'     
+        # Ensure that the 'pictures' directory exists, create it if it doesn't
+        os.makedirs("pictures", exist_ok=True)
         image_path = os.path.join("pictures", image_filename)
+        # Save the OpenCV image to the specified path
         cv2.imwrite(image_path, cv_image)
+        # Log the saved image's filename
         self.get_logger().info(f'Saved {image_filename}')
+        # move on to next image
         self.image_counter += 1
 
 def main(args=None):
+    # Initialize ROS2 Python library
     rclpy.init(args=args)
     node = CameraImageSubscriber()
+    # Spin node to keep it alive/responsive to incoming messages
     rclpy.spin(node)
+    # clean up/shutdown
     node.destroy_node()
     rclpy.shutdown()
 
