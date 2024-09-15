cc82b06410282565c4952c2e6ea01949b3bc54ec
mighT77 <142781273+mighT77@users.noreply.github.com>
2024-09-15 21:00:12 +0800

    Commenting

--- camera_calibration/GSMLCams/GSMLCams/sub_separate_calibrate.py
+++ camera_calibration/GSMLCams/GSMLCams/sub_separate_calibrate.py
@@ -11,8 +11,9 @@ def calibrate_camera(image_folder, chessboard_size, square_size_mm):
     # Scale object points to the actual size of the squares
     objp *= square_size_mm / 1000.0  # Convert from mm to meters
 
-    objpoints = []
-    imgpoints = []
+    # store object points and image points across mutliple aimges
+    objpoints = []            #3D points in real-world image
+    imgpoints = []            #2D points in image plane
 
     # Load images from the folder
     images = glob.glob(f'{image_folder}/*.jpg')
@@ -21,22 +22,27 @@ def calibrate_camera(image_folder, chessboard_size, square_size_mm):
         print("No images found in the specified directory.")
         return
 
+    # Loop through each image to find board corners
     for fname in images:
         img = cv2.imread(fname)
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Find the chessboard corners
+        # Find corners
         ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
 
         if ret:
+            # If corners found, add object and image points
             objpoints.append(objp)
             imgpoints.append(corners)
+
+            #Draw and display corners
             cv2.drawChessboardCorners(img, chessboard_size, corners, ret)
             cv2.imshow('img', img)
-            cv2.waitKey(500)
+            cv2.waitKey(500)            #Display for 500ms
 
     cv2.destroyAllWindows()
 
+    # Ensure corners were found in atleast 1 image
     if len(objpoints) == 0 or len(imgpoints) == 0:
         print("Chessboard corners not found in any images.")
         return
@@ -45,10 +51,11 @@ def calibrate_camera(image_folder, chessboard_size, square_size_mm):
     ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
 
     if not ret:
+        
         print("Camera calibration failed.")
         return
 
-    # Save the calibration results to a text file
+    # Save the calibration results - cam matrix and distortion coefficients - to a text file
     with open('camera_calibration_results.txt', 'w') as f:
         f.write("Camera Matrix:\n")
         np.savetxt(f, camera_matrix, fmt='%.6f')
